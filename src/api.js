f2655469826e69fd5035bf4da37da4fbb637f0cf
KailasMahavarkar <kailashmahavarkar5@gmail.com>
2022-12-04 20:45:18 +0530

    updated environment variables

--- src/api.js
+++ src/api.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { handleNetworkError, isNetworkError } from "./helper";
 
+const MODE = process.env.MODE || "dev"
+const SERVER_URL = process.env.SERVER_URL || 'https://unitybackend.up.railway.app'
+
+const API_URL = MODE === "dev" ? "http://localhost:2000" : SERVER_URL
+
 export const api = axios.create({
-    baseURL: 'http://localhost:2000'
+    baseURL: API_URL
 })
 
 
 // this interceptor will handle network related errors
 api.interceptors.response.use((response) => response, (error) => {
     // check network error
-    if (isNetworkError(error)){
+    if (isNetworkError(error)) {
         return handleNetworkError();
     }
 });
